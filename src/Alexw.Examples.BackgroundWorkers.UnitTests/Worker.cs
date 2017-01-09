5f7638a554aa01daf1646924e982a52bc1d915d5
alex <alex@alexw.co.uk>
2017-01-09 21:30:32 +0000

    basic testing of worker in another thread

---
+++ src/Alexw.Examples.BackgroundWorkers.UnitTests/Worker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Alexw.Examples.BackgroundWorkers.UnitTests
+{
+    public class Worker
+    {
+        private Timer _timer;
+
+        public DateTime LastSignalTime { get; private set; }
+
+        public void Start()
+        {
+            _timer = new Timer
+            {
+                Interval = TimeSpan.FromSeconds(1).TotalMilliseconds,
+                AutoReset = true,
+                Enabled = true
+            };
+
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.Start();
+
+            Thread.Sleep(-1);
+        }
+
+        public void Stop()
+        {
+            _timer.Elapsed -= OnTimerElapsed;
+            _timer.Stop();
+        }
+
+        private void OnTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            LastSignalTime = e.SignalTime;
+            Console.WriteLine(e.SignalTime + " Timer Elapsed Event");
+        }
+    }
+}
---
+++ src/Alexw.Examples.BackgroundWorkers.UnitTests/WorkerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Alexw.Examples.BackgroundWorkers.UnitTests
+{
+    [TestFixture]
+    public class WorkerTests
+    {
+        [Test]
+        public void WorkerRunningInBackgroundThread_TerminatesProperly()
+        {
+            var worker = new Worker();
+            var thread = new Thread(() =>
+            {
+                worker.Start();
+            });
+            thread.Start();
+
+            var fiveSecondsFromNow = DateTime.UtcNow.AddSeconds(5);
+            DoUntil(() => worker.LastSignalTime > fiveSecondsFromNow, (i, t) => TimeSpan.Zero, (i, t) => i < 100);
+            Assert.True(worker.LastSignalTime > fiveSecondsFromNow);
+        }
+
+        /// <summary>
+        /// Loop until a condition is met and your function returns true
+        /// </summary>
+        /// <param name="action">The action to perform. Return true to exit the loop.</param>
+        /// <param name="delay">Return a timespan for the delay between loops</param>
+        /// <param name="whentoStop">Return true when the loop should stop when catching exceptions or looping</param>
+        public static void DoUntil(Func<bool> action, Func<int, TimeSpan, TimeSpan> delay, Func<int, TimeSpan, bool> whentoStop)
+        {
+            var startedUtc = DateTime.UtcNow;
+            var amountOfRetriesAllowed = 0;
+            while (true)
+            {
+                try
+                {
+                    if (action()) return;
+                }
+                catch
+                {
+                    if (whentoStop(amountOfRetriesAllowed, DateTime.UtcNow.Subtract(startedUtc))) throw;
+                }
+                finally
+                {
+                    amountOfRetriesAllowed++;
+                }
+
+                var calculatedDelay = delay(amountOfRetriesAllowed, DateTime.UtcNow.Subtract(startedUtc));
+                if (calculatedDelay.TotalMilliseconds < 0)
+                {
+                    calculatedDelay = TimeSpan.Zero;
+                }
+                Thread.Sleep(calculatedDelay);
+            }
+        }
+    }
+}
